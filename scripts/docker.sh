ebb1c818e7d02765fb663fb4fa0394077561dbed
Patrick <plynch@sharpencx.com>
2020-07-06 14:49:12 -0400

    updating deprecated eslint

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 set -e
 
-PACKAGE_VERSION=$( node scripts/details.js VERSION )
+PACKAGE_VERSION=$(node scripts/details.js VERSION)
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+# Get local vars for scripts
+if [ -f .env ]; then
+    source .env
+fi
+
+if [ "${REPO_NAMESPACE}" == "" ]; then
+    REPO_NAMESPACE="deepstreamio"
+fi
+
+if [ "${REPO_NAME}" == "" ]; then
+    REPO_NAME="deepstream.io"
+fi
+
+echo "REPO_NAMESPACE=${REPO_NAMESPACE}"
+echo "REPO_NAME=${REPO_NAME}"
+
+if [ "${REPO_NAMESPACE}" == "deepstreamio" ]; then
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+fi
 
 npm i
 npm run tsc
 cd dist
 
-docker build . -t deepstreamio/deepstream.io:${PACKAGE_VERSION} -t deepstreamio/deepstream.io:latest
-docker push deepstreamio/deepstream.io:${PACKAGE_VERSION}
-docker push deepstreamio/deepstream.io:latest
+docker build . -t ${REPO_NAMESPACE}/${REPO_NAME}:${PACKAGE_VERSION} -t ${REPO_NAMESPACE}/${REPO_NAME}:latest
+docker push ${REPO_NAMESPACE}/${REPO_NAME}:${PACKAGE_VERSION}
+docker push ${REPO_NAMESPACE}/${REPO_NAME}:latest
 
 npm uninstall --save uWebSockets.js
 echo 'Replacing node with node-alpine'
 sed -i 's@node:12@node:12-alpine@' Dockerfile
 echo 'Building node alpine'
-docker build . -t deepstreamio/deepstream.io:${PACKAGE_VERSION}-alpine -t deepstreamio/deepstream.io:latest-alpine
+docker build . -t ${REPO_NAMESPACE}/${REPO_NAME}:${PACKAGE_VERSION}-alpine -t ${REPO_NAMESPACE}/${REPO_NAME}:latest-alpine
 echo 'Pushing node alpine'
-docker push deepstreamio/deepstream.io:${PACKAGE_VERSION}-alpine 
-docker push deepstreamio/deepstream.io:latest-alpine
+docker push ${REPO_NAMESPACE}/${REPO_NAME}:${PACKAGE_VERSION}-alpine
+docker push ${REPO_NAMESPACE}/${REPO_NAME}:latest-alpine
 
 cd ../
